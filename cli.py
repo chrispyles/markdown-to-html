f14a8bfe48939c428a6b1b73d7a01d13544e9d13
Chris Pyles <cpyles@berkeley.edu>
2019-08-07 15:01:21 -0700

    added nav

--- cli.py
+++
@@ -1,6 +0,0 @@
-import sys
-import argparse
-
-parser = argparse.ArgumentParser(description="Convert Markdown to HTML.")
-parser.add_argument("-n", "--nav", dest="nav", help="Add a JSON file to use for navigation menu")
-parser.add_argument("files", nargs=argparse.REMAINDER, help="Files to be converted to HTML")
