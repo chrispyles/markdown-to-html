934752cb0b423bae7cd926231e9fb7898557ca2a
Chris Pyles <cpyles@berkeley.edu>
2019-08-07 00:17:54 -0700

    added * recognition

--- converter.py
+++ converter.py
@@ -5,15 +5,20 @@
 
 import markdown2 as md
 import sys
+import glob
 
 if len(sys.argv) == 1:
 	print("Please enter a filename.")
+else if len(sys.argv) == 2 and "*" in sys.argv[2]:
+	file_names = glob.glob(sys.argv[2])
 else:
-	for file_name in sys.argv[1:]:
-		markdowner = md.Markdown()
+	file_names = sys.argv[1:]
 
-		with open(file_name) as f:
-			html = markdowner.convert(f.read())
+for file_name in file_names:
+	markdowner = md.Markdown()
 
-		with open(file_name[:-2] + "html", "w+") as f:
-			f.write(html)
+	with open(file_name) as f:
+		html = markdowner.convert(f.read())
+
+	with open(file_name[:-2] + "html", "w+") as f:
+		f.write(html)
